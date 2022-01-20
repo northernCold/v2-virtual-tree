a238b5f7dadcf4d9296653faad8b85ec191dcbce
黄晓东 <1357805398@qq.com>
2022-01-20 15:51:31 +0800

    refactor(transition): remove velocity-animate
    重构后
    1.在大量数据下展开/收起时，动画更加流畅
    2.快速点击展开/收起时，动画更正常

--- src/components/virtual-list/collapse-transition.vue
+++ src/components/virtual-list/collapse-transition.vue
@@ -1,78 +1,171 @@
 <template>
   <transition
-    :css="false"
+    :name="name"
     appear
-    @before-enter="beforeEnter"
-    @enter="enter"
+    @leave="handleLeave"
+    @enter="handleEnter"
+    @before-leave="handleBeforeLeave"
+    @after-leave="handleAfterLeave"
+    @after-enter="handleAfterEnter"
   >
     <slot></slot>
   </transition>
 </template>
 <script>
 import Store from "../../store";
-import Velocity from 'velocity-animate';
 
 export default {
+  name: 'CollapseTransition',
   props: {
+    width: {
+      type: Boolean,
+      default: false,
+    },
     mode: {
       type: String,
-      default: ''
+      default: "",
     },
     motionHeight: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
+    // reverse: {
+    //   type: Boolean
+    // }
+  },
+  computed: {
+    name() {
+      return this.reverse
+        ? "fade-in-height-collapse"
+        : "fade-in-height-expand";
+    },
+    reverse() {
+      return Store.state.transitionMode !== "expand";
+    },
   },
   methods: {
-    customAfterAppearHook() {
+    handleBeforeLeave(el) {
+      if (this.width) {
+        el.style.maxWidth = `${el.offsetWidth}px`;
+      } else {
+        el.style.maxHeight = "0";
+        el.style.height = "0";
+        el.style.overflow = "hidden";
+      }
+      void el.offsetWidth;
     },
-    beforeEnter(el) {
-      if (this.mode === 'expand') {
-        el.style.height = '0';
-      }
-      if (this.mode === 'collapse') {
-        el.style.height = `${this.motionHeight}px` ;
-        el.style.overflow = 'hidden';
-        el.style.opacity = 1;
+    handleLeave(el) {
+      if (this.width) {
+        el.style.maxWidth = "0";
+      } else {
+        el.style.maxHeight = "0";
       }
     },
-
-    enter(el, done) {
-      var delay = 200;
-      if (this.mode === 'expand') {
-        el.style.overflow = 'hidden';
-        setTimeout(function () {
-          Velocity(
-            el,
-            { opacity: 1, height:  el.scrollHeight + 'px' },
-            { complete: () => {
-              done();
-              Store.mutations.setAnimating(false);
-            } }
-          )
-        }, delay)
+    handleAfterLeave(el) {
+      if (this.width) {
+        el.style.maxWidth = "";
+      } else {
+        el.style.maxHeight = "";
       }
-      if (this.mode === 'collapse') {
-        setTimeout(function () {
-          Velocity(
-            el,
-            { height: 0, opacity: 0 },
-            { complete: () => {
-              done();
-              Store.mutations.setAnimating(false);
-            } }
-          )
-        }, delay)
+      void el.offsetWidth;
+    },
+    handleEnter(el) {
+      console.log(Store.state.animating);
+      el.style.transition = "none";
+      if (this.width) {
+        const memorizedWidth = el.offsetWidth;
+        el.style.maxWidth = "0";
+        void el.offsetWidth;
+        el.style.transition = "";
+        el.style.maxWidth = `${memorizedWidth}px`;
+      } else {
+        if (this.reverse) {
+          el.style.maxHeight = `${el.offsetHeight}px`;
+          void el.offsetHeight;
+          el.style.transition = "";
+          el.style.maxHeight = "0";
+        } else {
+          const memorizedHeight = el.offsetHeight;
+          el.style.maxHeight = "0";
+          void el.offsetWidth;
+          el.style.transition = "";
+          el.style.maxHeight = `${memorizedHeight}px`;
+        }
       }
-    }
-  }
+    },
+    handleAfterEnter(el) {
+      if (this.width) {
+        el.style.maxWidth = "";
+      } else {
+        if (!this.reverse) {
+          el.style.maxHeight = "";
+        }
+      }
+      Store.mutations.setAnimating(false);
+    },
+  },
 };
 </script>
 <style lang="scss">
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .2s;
+.fade-in-height {
+  &-collapse {
+    &-enter, &-leave-to {
+      opacity: 1;
+    }
+    &-enter-to, &-leave {
+      opacity: 0;
+      margin-top: 0 !important;
+      margin-bottom: 0 !important;
+    }
+    &-enter-active {
+      overflow: hidden;
+      transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s,
+        opacity 0.2s cubic-bezier(0, 0, 0.2, 1) 0s,
+        margin-top 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s,
+        margin-bottom 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s,
+        padding-top 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s,
+        padding-bottom 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+    }
+    &-leave-active {
+      overflow: hidden;
+      transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1),
+        opacity 0.2s cubic-bezier(0.4, 0, 1, 1),
+        margin-top 0.2s cubic-bezier(0.4, 0, 0.2, 1),
+        margin-bottom 0.2s cubic-bezier(0.4, 0, 0.2, 1),
+        padding-top 0.2s cubic-bezier(0.4, 0, 0.2, 1),
+        padding-bottom 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+    }
+  }
+  &-expand {
+    &-leave, &-enter-to {
+      opacity: 1;
+    }
+    &-leave-to, &-enter {
+      opacity: 0;
+      margin-top: 0 !important;
+      margin-bottom: 0 !important;
+    }
+    &-leave-active {
+      overflow: hidden;
+      transition:
+        max-height 0.2s cubic-bezier(.4, 0, .2, 1) 0s,
+        opacity 0.2s cubic-bezier(0, 0, .2, 1) 0s,
+        margin-top 0.2s cubic-bezier(.4, 0, .2, 1) 0s,
+        margin-bottom 0.2s cubic-bezier(.4, 0, .2, 1) 0s,
+        padding-top 0.2s cubic-bezier(.4, 0, .2, 1) 0s,
+        padding-bottom 0.2s cubic-bezier(.4, 0, .2, 1) 0s
+    }
+    &-enter-active {
+      overflow: hidden;
+      transition:
+        max-height 0.2s cubic-bezier(.4, 0, .2, 1),
+        opacity 0.2s cubic-bezier(.4, 0, 1, 1),
+        margin-top 0.2s cubic-bezier(.4, 0, .2, 1),
+        margin-bottom 0.2s cubic-bezier(.4, 0, .2, 1),
+        padding-top 0.2s cubic-bezier(.4, 0, .2, 1),
+        padding-bottom 0.2s cubic-bezier(.4, 0, .2, 1)
+    }
+  }
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
+
 </style>
